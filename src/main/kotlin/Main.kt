ffec027dd6a5818e8acb41c3700b7eaac09b1cd1
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-10-05 13:53:28 +0700

    KS-1-2 Created folder for task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,5 @@
 package org.example
 
 fun main() {
-    val ordersCount: UByte = 75u
-    val gratitudeMessage: String = "Thanks for your purchase!"
-    var workersCount: UShort = 2000u
 
-    println("Orders count: ${ordersCount}")
-    println("Gratitude message: ${gratitudeMessage}")
-//    println("Workers count: ${workersCount}")
-
-    workersCount = 1999u
-
-    print("Actual workers count: ${workersCount}")
 }
