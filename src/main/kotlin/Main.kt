77932da703b7a1dbde7931d2625f0691ba021f03
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-10-06 08:48:48 +0700

    KS-1-1 Refactor task1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 package org.example
 
 fun main() {
-    val ordersCount: UByte = 75u
-    val gratitudeMessage: String = "Thanks for your purchase!"
+
 }
---
+++ src/main/kotlin/lesson1/task1.kt
@@ -0,0 +1,6 @@
+package org.example.lesson1
+
+fun main() {
+    val ordersCount: UByte = 75u
+    val gratitudeMessage: String = "Thanks for your purchase!"
+}
