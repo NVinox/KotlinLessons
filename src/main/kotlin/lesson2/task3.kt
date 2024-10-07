2c45068a88d928de344b08b108208eff217d70a5
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-10-07 23:20:11 +0700

    KS-2-3 Fixed format time to train timetable

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -1,37 +1,19 @@
 package org.example.lesson2
 
-const val MINIMUM_SECONDS: Byte = 10
 const val MINUTES_IN_HOUR: Byte = 60
 const val HOURS_IN_DAY = 24
 
-const val HOUR_DEPARTURE: Byte = 9
-const val MINUTE_DEPARTURE: Byte = 39
-const val TIME_TRAVEL: Short = 457
-
 fun main() {
-    val fullMinutesArrival: Int = HOUR_DEPARTURE * MINUTES_IN_HOUR + MINUTE_DEPARTURE + TIME_TRAVEL
-    val hoursArrival: Byte = calculateHoursArrival(fullMinutesArrival)
-    val minutesArrival: String = calculateMinutesArrival(fullMinutesArrival)
-
-    print("${hoursArrival}:${minutesArrival}")
-}
-
-fun calculateHoursArrival(minutes: Int): Byte {
-    val hoursArrival: Int = minutes / MINUTES_IN_HOUR
+    val hourDeparture: Byte = 9
+    val minuteDeparture: Byte = 39
+    val timeTravel: Short = 457
+    val fullMinutesArrival: Int = hourDeparture * MINUTES_IN_HOUR + minuteDeparture + timeTravel
+    var hoursArrival: Byte = (fullMinutesArrival / MINUTES_IN_HOUR).toByte()
+    val minutesArrival: Int = fullMinutesArrival % MINUTES_IN_HOUR
 
     if (hoursArrival % HOURS_IN_DAY != 0) {
-        return (hoursArrival % HOURS_IN_DAY).toByte()
+        hoursArrival = (hoursArrival % HOURS_IN_DAY).toByte()
     }
 
-    return hoursArrival.toByte()
-}
-
-fun calculateMinutesArrival(minutes: Int): String {
-    val minutesArrival: Int = minutes % MINUTES_IN_HOUR
-
-    if (minutesArrival < MINIMUM_SECONDS) {
-        return "0${minutesArrival}"
-    }
-
-    return minutesArrival.toString()
+    print("%02d:%02d".format(hoursArrival, minutesArrival))
 }
